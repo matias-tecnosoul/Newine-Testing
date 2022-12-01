9c324fe9c9ef7fbc87e6dee67d8060f2cdd673a9
matias-tecnosoul <matias@tecnosoul.com.ar>
2022-12-01 11:11:24 -0300

    funcionando qrscanner sin fondo css

--- www/js/index.js
+++ www/js/index.js
@@ -10,47 +10,90 @@ function onDeviceReady() {
    // window.addEventListener('message', scan, true);
     //
    // document.getElementById("getPosition").addEventListener("click", getPosition);
+   document.getElementById("prepareButton").addEventListener("click", prepare);
    document.getElementById("scanButton").addEventListener("click", scan);
+   document.getElementById("mostrarButton").addEventListener("click", mostrar);
+   document.getElementById("hideButton").addEventListener("click", hide);
    document.getElementById("stopButton").addEventListener("click", stopscan);
 
  }
 
+function prepare(){
+QRScanner.prepare(onDone); // show the prompt
 
+function onDone(err, status){
+  if (err) {
+   // here we can handle errors and clean up any loose ends.
+   console.error(err);
+  }
+  if (status.authorized) {
+    // W00t, you have camera access and the scanner is initialized.
+    // QRscanner.show() should feel very fast.
+  } else if (status.denied) {
+   // The video preview will remain black, and scanning is disabled. We can
+   // try to ask the user to change their mind, but we'll have to send them
+   // to their device settings with `QRScanner.openSettings()`.
+  } else {
+    // we didn't get permission, but we didn't get permanently denied. (On
+    // Android, a denial isn't permanent unless the user checks the "Don't
+    // ask again" box.) We can ask again at the next relevant opportunity.
+  }
+}
+}
 function scan() {
 //QRScanner.scan(displayContents);
 //window.QRScanner.show();
 //alert(2 + 6);
   //   alert('scanbegin');
-       alert ('adentroScan ');
+      // alert ('adentroScan ');
 
-     window.QRScanner.scan(displayContents);
-
+     QRScanner.scan(displayContents);
      // Make the webview transparent so the video preview is visible behind it.
-     window.QRScanner.show();
+     //QRScanner.show();
+     QRScanner.show(function(status){
+       console.log(status);
+     });
      // Be sure to make any opaque HTML elements transparent here to avoid
      // covering the video.
 
 
 }
+function mostrar() {
+     QRScanner.show(function(status){
+       console.log(status);
+     });
+     // Be sure to make any opaque HTML elements transparent here to avoid
+     // covering the video.
+}
+function hide() {
+     QRScanner.hide(function(status){
+       console.log(status);
+     });
+     
+     // Be sure to make any opaque HTML elements transparent here to avoid
+     // covering the video.
+}
 function stopscan() {
-         alert ('adentroStopScan ');
-
-    window.QRScanner.cancelScan(function(status){
-  console.log(status);
-    }
-
+     //    alert ('adentroStopScan ');
+         QRScanner.cancelScan(function(status){
+             console.log(status);
+             console.error(err._message);
+         }
 );
 
 }
 
-function displayContents(err, text){
-     alert ('adentrodisplayContents ');
+function displayContents(err, contents){
+    // alert ('adentrodisplayContents ');
 
   if(err){
+  console.error(err._message);
+  console.log(status);
     // an error occurred, or the scan was canceled (error code `6`)
   } else {
+  console.log(status);
     // The scan completed, display the contents of the QR code:
-    alert(text);
+    alert('The QR Code contains: ' + contents);
   }
 }
 
